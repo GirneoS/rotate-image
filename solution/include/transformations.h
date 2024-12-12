93bd837618f71ca8bdcf12a1a3cf5e53f155d3e6
GirneoS <ozhegofff72@gmail.com>
2024-12-12 18:24:37 +0300

    remove "none" transformation

--- solution/include/transformations.h
+++ solution/include/transformations.h
@@ -9,6 +9,5 @@ void flip_v(struct image* init_img, struct image* transformed_img);
 void flip_h(struct image* init_img, struct image* transformed_img);
 void cw_90(struct image* init_img, struct image* transformed_img);
 void ccw_90(struct image* init_img, struct image* transformed_img);
-void none(struct image* init_img, struct image* transformed_img);
 
 #endif //IMAGE_TRANSFORM_TRANSFORMATIONS_H
