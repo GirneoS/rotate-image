94c92dfce46a5d6ac4aa30fbb1f88fae38222b0d
GirneoS <ozhegofff72@gmail.com>
2025-01-14 23:06:57 +0300

    :)

--- solution/include/transformations.h
+++ solution/include/transformations.h
@@ -5,9 +5,9 @@
 #ifndef IMAGE_TRANSFORM_TRANSFORMATIONS_H
 #define IMAGE_TRANSFORM_TRANSFORMATIONS_H
 
-void flip_v(struct image* init_img, struct image* transformed_img);
-void flip_h(struct image* init_img, struct image* transformed_img);
-void cw_90(struct image* init_img, struct image* transformed_img);
-void ccw_90(struct image* init_img, struct image* transformed_img);
+struct transformation_result flip_v(struct image* init_img, struct image* transformed_img);
+struct transformation_result flip_h(struct image* init_img, struct image* transformed_img);
+struct transformation_result cw_90(struct image* init_img, struct image* transformed_img);
+struct transformation_result ccw_90(struct image* init_img, struct image* transformed_img);
 
 #endif //IMAGE_TRANSFORM_TRANSFORMATIONS_H
